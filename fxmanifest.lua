9c54379efd529da51ae862b43992267739157239
MertCxn <canmert039@gmail.com>
2025-04-13 03:27:31 +0300

    Add files via upload
    Self Storage Resource for Your Fivem Server

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+lua54 'yes' -- needed for reaper
+fx_version "adamant"
+game "gta5"
+client_scripts {
+    "stash.lua"
+}
+server_scripts {
+    "@oxmysql/lib/MySQL.lua",
+}
---
+++ stash.lua
@@ -0,0 +1,39 @@
+QBCore = nil
+Citizen.CreateThread(function() 
+    while QBCore == nil do
+        TriggerEvent("QBCore:GetObject", function(obj) QBCore = obj end)    
+        Citizen.Wait(200)
+    end
+end)
+
+QBCore = exports['qb-core']:GetCoreObject() 
+
+KisiselDepolar = {
+    vector3(265.89, -999.36, -99.01),
+},
+
+CreateThread(function()
+    while true do
+        sleep = 100
+        local plyPed = PlayerPedId()
+        local plyCoords = GetEntityCoords(plyPed)
+        
+        for k, v in pairs(KisiselDepolar) do
+            local dist = #(plyCoords - v)
+
+            if dist < 2.5 then
+                sleep = 3
+                QBCore.Functions.DrawText3D(v.x, v.y, v.z, "[E] Kisisel Depo")
+                if IsControlJustPressed(0, 38) then
+                    local cid = QBCore.Functions.GetPlayerData().citizenid
+                    TriggerServerEvent("inventory:server:OpenInventory", "stash", "Kişisel Depo - "..cid,  {
+                        maxweight = 5000000000,
+                        slots = 10000,
+                        owner = cid
+                    })
+                end
+            end
+        end
+        Wait(sleep)
+    end
+end)
